2939278fc732fb20c40f3a692c17268af3307568
Alexey <lipilinalexey@gmail.com>
2023-03-17 00:38:27 +0300

    add: database and Docker

--- stat/stat-server/src/main/java/ru/practicum/stat/service/HitService.java
+++ stat/stat-server/src/main/java/ru/practicum/stat/service/HitService.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 public interface HitService {
     void create(HitDto hitDto);
+
     Collection<StatsDto> get(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
